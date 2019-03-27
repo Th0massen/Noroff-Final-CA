7c31ce5a0b899b3ecdc9455d70bd28c15c4f482e
Thomassen <noveng3ance@gmail.com>
2019-03-27 15:09:15 +0100

    media queries

--- noroff_ca2/src/components/header-component/header-component.js
+++ noroff_ca2/src/components/header-component/header-component.js
@@ -4,6 +4,7 @@ import styles from './header.module.scss'
 const HeaderComponent = () => {
     return(
         <div className={styles.header}>
+            <img src="../../assets/magiclogo.png" alt="Logo"/>
         </div>
     )
 }
