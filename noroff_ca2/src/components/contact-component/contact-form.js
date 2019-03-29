f00878f7c2a7f98b88d24455485407a152c70dcb
Thomassen <noveng3ance@gmail.com>
2019-03-29 15:08:31 +0100

    completed about us page
    completes about us page, and almost done with contact page

--- noroff_ca2/src/components/contact-component/contact-form.js
+++ noroff_ca2/src/components/contact-component/contact-form.js
@@ -1,13 +1,58 @@
 import React from 'react'
+import styles from './contact-component.module.scss'
 
-const ContactForm = () => {
+const ContactForm = ( props ) => {
+
+    let nameError = null;
+    let phoneError = null;
+    let mailError = null;
+    let success = ''
+
+    if( props.nameError === false ){
+        nameError = (
+            <React.Fragment>
+                <p>Enter a Name!</p>
+            </React.Fragment>
+        )
+    } else{
+        nameError = null;
+    }
+    if( !props.numbError ){
+        phoneError = (
+            <React.Fragment>
+                <p>Invalid Number!</p>
+            </React.Fragment>
+        )
+    } else {
+        phoneError = null;
+    }
+    if( !props.mailError ){
+        mailError = (
+            <React.Fragment>
+                <p>Invalid Email</p>
+            </React.Fragment>
+        )
+    } else {
+        mailError = null;
+    }
+
+    if( props.success ){
+            success = 'Success'
+    }
+
     return(
-        <form>
-            <input type="text" placeholder="First Name" /><br/>
-            <input type="text" placeholder="Last Name" /><br/>
-            <input type="text" placeholder="Phone Number" pattern="[0-9]{8}"/><br/>
-            <input type="email" placeholder="Email Address"/><br/>
-
+        <form className={ styles.form }>
+            <h1 className={ styles.form_title }>Contact us</h1>
+            <input className={ styles.form_input } type="text" placeholder="Full Name" onChange={ props.checkName } required /><br/>
+            { nameError }
+            <input className={ styles.form_input } type="text" placeholder="Phone Number" onChange={ props.checkPhone } required/><br/>
+            { phoneError }
+            <input className={ styles.form_input } type="email" placeholder="Email Address" onChange={ props.checkEmail } required/><br/>
+            { mailError }
+            <input className={ styles.form_button } type="button" value='Submit' onClick={ props.handleSubmit }/>
+            <div>
+                <p>{ success }</p>
+            </div>
         </form>
     )
 }
--- noroff_ca2/src/pages/ContactPage/ContactPage.js
+++ noroff_ca2/src/pages/ContactPage/ContactPage.js
@@ -4,12 +4,83 @@ import HeaderComponent from '../../components/header-component/header-component'
 import ContactForm from '../../components/contact-component/contact-form';
 
 class ContactPage extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            name: '',
+            phone: '',
+            email: '',
+            showNameErr: null,
+            showNumbErr: true,
+            showEmailErr: true,
+            success: false
+        }
+    }
+
+    onNameInput = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+
+    onPhoneInput = (e) => {
+        this.setState({
+            phone: e.target.value
+        })
+    }
+
+    onEmailInput = (e) => {
+        this.setState({
+            email: e.target.value
+        })
+    }
+
+    onSubmit = () => {
+        if( this.state.showNameErr === true && this.state.showNumbErr === true && this.state.showEmailErr === true ){
+            this.setState({
+                success: true
+            })
+        }
+        const phoneRegex = new RegExp(/([0-9]{3})+[. -]([0-9]{3})+[. -]([0-9]{4})/)
+        const mailRegex = new RegExp(/[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$/)
+        const phoneTest = phoneRegex.test(this.state.phone)
+        const emailTest = mailRegex.test(this.state.email)
+        console.log(phoneTest, emailTest)
+        if( this.state.name === '' || null ){
+            this.setState({
+                showNameErr: false
+            })
+        } else{
+            this.setState({
+                showNameErr: true
+            })
+        }
+        this.setState({
+            showNumbErr: phoneTest,
+            showEmailErr: emailTest
+        })
+        if( this.state.showNameErr === true && this.state.showNumbErr === true && this.state.showEmailErr === true ){
+            this.setState({
+                success: true
+            })
+        }
+    }
+
     render(){
         return(
             <div>
                 <MenuComponent/>
                 <HeaderComponent/>
-                <ContactForm/>
+                <ContactForm
+                    nameError = { this.state.showNameErr }
+                    numbError = { this.state.showNumbErr }
+                    mailError = { this.state.showEmailErr }
+                    checkName = { this.onNameInput }
+                    checkPhone = { this.onPhoneInput }
+                    checkEmail = { this.onEmailInput }
+                    handleSubmit = { this.onSubmit }
+                    success = { this.state.success }
+                />
             </div>
         )
     }
